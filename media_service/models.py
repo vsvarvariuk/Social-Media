8915e8bf2b075984ee078457e5d9222a517a37c9
vavarvariuk <vasya.varvariuk.27@gmail.com>
2025-03-21 13:38:39 +0200

    fix models & add serializers for media_service app

--- media_service/models.py
+++ media_service/models.py
@@ -1,21 +1,19 @@
 import pathlib
 import uuid
-
-from django.conf import settings
 from django.db import models
 from django.utils.text import slugify
 
 
 def upload_to(instance, filename):
-    new_file_name = (f"{slugify(instance.user.first_name)-(uuid.uuid4())}"
+    new_file_name = (f"{slugify(instance.first_name)-(uuid.uuid4())}"
     + pathlib.Path(filename).suffix)
     return f"uploads/profile/{new_file_name}"
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE,
-                                related_name="profiles")
+    first_name = models.CharField(max_length=155)
+
+    last_name = models.CharField(max_length=155)
 
     profile_picture = models.ImageField(upload_to=upload_to,
                                         blank=True,
@@ -37,9 +35,6 @@ def upload_to_post(instance, filename):
 
 class Post(models.Model):
     content = models.TextField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE,
-                                related_name="posts")
 
     image = models.ImageField(upload_to=upload_to_post,
                               blank=True,
@@ -48,42 +43,30 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return (f"{self.user.first_name} {self.user.last_name}"
-                f" ({self.created_at})")
+        return self.created_at
 
 
 class Like(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE,
-                                related_name="posts")
 
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
 
 
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,
-                                on_delete=models.CASCADE,
-                                related_name="posts")
-    text = models.TextField()
+    comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.created_at
 
 
 class Follow(models.Model):
-    follower = models.ForeignKey(settings.AUTH_USER_MODEL,
-                                 on_delete=models.CASCADE,
-                                 related_name="following")
-
-    followed = models.ForeignKey(settings.AUTH_USER_MODEL,
-                                 on_delete=models.CASCADE,
-                                 related_name="followers")
+    followed_profile = models.ForeignKey(Profile,
+                                         on_delete=models.CASCADE,
+                                         related_name="followers")
 
     created_at = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=["follower", "followed"], name="unique_follow")
-        ]
-
     def __str__(self):
-        return f"{self.follower} follows {self.followed}"
+        return (f"Follow {self.u} "
+                f"{self.followed_profile.last_name} from {self.created_at}")
---
+++ media_service/serializers.py
@@ -0,0 +1,37 @@
+from rest_framework import serializers
+
+from media_service.models import Profile, Post, Like, Comment, Follow
+
+class ProfileSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Profile
+        fields = ("id", "profile_picture", "birth_day", "location", "created_at")
+
+
+class PostSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Post
+        fields = ("id", "content", "image", "created_at")
+
+
+class LikeSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Like
+        fields = ("id", "post")
+
+
+class CommentSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Comment
+        fields = ("id", "comment", "created_at")
+
+
+class FollowSerializers(serializers.ModelSerializer):
+
+    class Meta:
+        model = Follow
+        fields = ("id", "followed_profile", "created_at")
